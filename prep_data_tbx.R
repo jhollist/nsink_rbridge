2aad5feed17156a96273eed9eb63e87bcdfe7ad7
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-09-18 13:17:31 -0400

    draft prep data tbx script

--- prep_data_tbx.R
+++ prep_data_tbx.R
@@ -1,5 +1,18 @@
 tool_exec <- function(in_params, out_params){
-  search_string <- in_params$name
-  result_df <- nsink::nsink_get_huc_id(search_string)
-  print(result_df)
+  huc <- in_params$huc
+  prj <- in_params$projection
+  data <- in_params$data_dir
+  output_folder <- out_params$output
+  prepped_data <- nsink::nsink_prep_data(huc,prj,data)
+  sf::st_write(prepped_data$streams, paste0(output_folder,"/streams.shp"))
+  sf::st_write(prepped_data$streams, paste0(output_folder,"/lakes.shp"))
+  raster::writeRaster(prepped_data$fdr, "fdr.tif")
+  raster::writeRaster(prepped_data$impervious, "impervious.tif")
+  sf::st_write(prepped_data$ssurgo, paste0(output_folder,"/ssurgo.shp"))
+  readr::write_csv(prepped_data$q, paste0(output_folder,"/q.csv"))
+  readr::write_csv(prepped_data$tot, paste0(output_folder,"/tot.csv"))
+  readr::write_csv(prepped_data$lakemorpho, 
+                   paste0(output_folder,"/lakemorpho.csv"))
+  sf::st_write(prepped_data$huc, paste0(output_folder,"/huc.shp"))
+  raster::writeRaster(prepped_data$raster_template, "raster_template.tif")
 }

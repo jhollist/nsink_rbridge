fa9b428a40a1e689936ea4ce7f057af3db055f91
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-09-18 11:45:45 -0400

    skeleton of scripts

---
+++ get_raw_data_tbx.R
@@ -0,0 +1,6 @@
+tool_exec <- function(in_params, out_params){
+  huc <- in_params$huc
+  output_folder <- in_params$output_folder
+  result_df <- nsink::nsink_get_data(huc, output_folder)
+  print(result_df)
+}

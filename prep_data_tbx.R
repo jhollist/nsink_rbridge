e1e5dcf624cac8547f4d94fafa4eacf418da2a2a
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-09-18 13:23:43 -0400

    consistency in naming

--- prep_data_tbx.R
+++ prep_data_tbx.R
@@ -1,18 +1,18 @@
-tool_exec <- function(in_params, out_params){
+tool_exec <- fuoutputnction(in_params, out_params){
   huc <- in_params$huc
-  prj <- in_params$projection
+  epsg <- in_params$epsg
   data <- in_params$data_dir
-  output_folder <- out_params$output
-  prepped_data <- nsink::nsink_prep_data(huc,prj,data)
-  sf::st_write(prepped_data$streams, paste0(output_folder,"/streams.shp"))
-  sf::st_write(prepped_data$streams, paste0(output_folder,"/lakes.shp"))
+  output <- out_params$output_dir
+  prepped_data <- nsink::nsink_prep_data(huc,epsg,data)
+  sf::st_write(prepped_data$streams, paste0(output,"/streams.shp"))
+  sf::st_write(prepped_data$streams, paste0(output,"/lakes.shp"))
   raster::writeRaster(prepped_data$fdr, "fdr.tif")
   raster::writeRaster(prepped_data$impervious, "impervious.tif")
-  sf::st_write(prepped_data$ssurgo, paste0(output_folder,"/ssurgo.shp"))
-  readr::write_csv(prepped_data$q, paste0(output_folder,"/q.csv"))
-  readr::write_csv(prepped_data$tot, paste0(output_folder,"/tot.csv"))
+  sf::st_write(prepped_data$ssurgo, paste0(output,"/ssurgo.shp"))
+  readr::write_csv(prepped_data$q, paste0(output,"/q.csv"))
+  readr::write_csv(prepped_data$tot, paste0(output,"/tot.csv"))
   readr::write_csv(prepped_data$lakemorpho, 
-                   paste0(output_folder,"/lakemorpho.csv"))
-  sf::st_write(prepped_data$huc, paste0(output_folder,"/huc.shp"))
-  raster::writeRaster(prepped_data$raster_template, "raster_template.tif")
+                   paste0(output,"/lakemorpho.csv"))
+  sf::st_write(prepped_data$huc, paste0(output,"/huc.shp"))
+  raster::writeRaster(prepped_data$raster_template, paste0(output,"raster_template.tif"))
 }
